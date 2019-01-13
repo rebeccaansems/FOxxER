adc2c68ba7743aa85a4400c0a5675f96397bb552
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-01-12 23:27:14 -0400

    Smoothed camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 cameraPos;
+
+    private Vector3 targetPos;
+
+    void Update()
+    {
+        if (target)
+        {
+            targetPos = new Vector3(target.position.x + cameraPos.x, cameraPos.y, target.position.z + cameraPos.z);
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.1f);
+        }
+    }
+}
