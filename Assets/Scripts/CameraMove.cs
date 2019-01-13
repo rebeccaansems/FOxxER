c79346db01cf5e33d50567c14874e115914b432a
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-01-12 22:53:17 -0400

    Added camera movement

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float cameraSpeed;
+
+    private void Update()
+    {
+        this.transform.position = this.transform.position + Vector3.forward * Time.deltaTime * cameraSpeed;
+    }
+}
