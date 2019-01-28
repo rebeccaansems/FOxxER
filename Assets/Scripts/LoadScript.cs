b58249775228bf2589d2e07a9adee784111492b6
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-01-28 19:30:01 -0400

    Removed unused terrains and added new island chunk

---
+++ Assets/Scripts/LoadScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScript : MonoBehaviour
+{
+    public bool destroyLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (destroyLoad)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
