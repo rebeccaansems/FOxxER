ad8a57b8ec0e88062ce3c7f9802f61ef51f74810
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-01-30 17:22:46 -0400

    Added modern navmesh stuff

---
+++ Assets/Scripts/NavigationBaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationBaker : MonoBehaviour
+{
+    public NavMeshSurface[] surfaces;
+
+    private void Start()
+    {
+        foreach(NavMeshSurface currSurface in surfaces)
+        {
+            currSurface.BuildNavMesh();
+        }
+    }
+}
