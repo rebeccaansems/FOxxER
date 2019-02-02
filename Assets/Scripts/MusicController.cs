397164ae548b2febb4026f74ae48e53147c52d99
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-02 02:43:24 -0400

    Added music buttons

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour
+{
+    public Image soundOn, soundOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundOn.enabled = !OverallController.instance.isMuted;
+        soundOff.enabled = OverallController.instance.isMuted;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/OverallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverallController : MonoBehaviour
+{
+    public bool isMuted = false;
+
+    public static OverallController instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
