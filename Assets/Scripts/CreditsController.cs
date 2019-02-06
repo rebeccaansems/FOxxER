62883a4c7f5fc947be515cb5bcaf7d018ebc45dd
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-06 14:55:43 -0400

    Read credits from file

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour
+{
+    public TextAsset creditsFile;
+
+    private void Start()
+    {
+        this.GetComponent<TextMeshProUGUI>().text = creditsFile.text;
+    }
+}
